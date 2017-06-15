dbf98a0ec06dff459d2152eca578cecfb0961b8a
gabytrejo <gabytrejo303@gmail.com>
2017-06-15 11:02:55 +0000

    POST: tema agregado

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var api = {
 
 var cargarPagina = function(){
     cargarTemas();
+    $("#crearTema").click(crearTema);
+    $("#agregarTema").submit(agregarTema);
+    $("#busqueda").keyup(filtrarTemas);
 };
 
 var $listaTemas = $("#temas");
@@ -18,11 +21,12 @@ var verTema = function(tema){
     var autor = tema.author_name;
     var titulo = tema.content;
     var respuestas = tema.responses_count;
+    console.log(respuestas);
    
     //Creacion de fila
     var $tr = $("<tr />");
     //Celda de titulo
-    var $tituloTd= $("<td />");
+    var $tituloTd= $("<td />", {"class": "tituloTema"});
     $tituloTd.text(titulo);
     //Celda de autor
     var $autorTd= $("<td />");
@@ -30,8 +34,38 @@ var verTema = function(tema){
     // Celda de respuestas
     var $respuestasTd = $("<td />").text(respuestas);
     $tr.append($tituloTd).append($autorTd).append($respuestasTd);
-    $listaTemas.append($tr);
-    
+    $listaTemas.append($tr);   
+};
+
+var crearTema = function() {
+    $("#tarjetaAgregar").removeClass("hide");
+};
+
+var agregarTema = function(e) {
+    e.preventDefault();
+    var autor = $("#autorAgregar").val();
+    var titulo = $("#tituloAgregar").val();
+    var respuesta = 0;
+    $.post(api.url, {
+        author_name: autor,
+        content: titulo,
+        responses_count: respuesta
+    }, function(tema){
+        console.log(tema);
+        verTema(tema);
+        $("#tarjetaAgregar").addClass("hide");        
+    });
+};
+
+var filtrarTemas = function (e) {
+	e.preventDefault();
+	var criterioBusqueda = $("#buscar").val().toLowerCase();
+	var temasFiltrados = $(".tituloTema").filter(function (indice,tema) {
+        console.log(tema.innerHTML);
+        return tema.innerHTML;
+		//return tema.innerHTML.indexOf(criterioBusqueda) >= 0;
+	});
+	//imprimir(temasFiltrados);
 };
 
 
